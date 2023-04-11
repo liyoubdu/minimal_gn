7ef848a2751ac4641efea4025b3cd6b61878d4cb
liyou <liyoubdu@qq.com>
2023-04-11 22:14:41 +0800

    22

--- generate_world.py
+++ generate_world.py
@@ -2,10 +2,12 @@
 
 import os
 import sys
+import random
 
 out_dir = sys.argv[1]
 cc_file = sys.argv[2]
 h_file = sys.argv[3]
+randNum = random.randint(0, 54321)
 
 def write_if_changed(path, contents):
     if not os.path.exists(path):
@@ -25,7 +27,7 @@ write_if_changed(f'{out_dir}/{cc_file}', f'''\
 #include "{h_file}"
 
 const char *world() {{
-    return "world";
+    return "world-{randNum}";
 }}
 ''')
 
