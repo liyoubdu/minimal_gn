467d323f03c367a5e6bc4a1593daa4d96cc45a09
liyou <liyoubdu@qq.com>
2023-04-11 22:42:31 +0800

    333

--- generate_world.py
+++ generate_world.py
@@ -3,34 +3,24 @@
 import os
 import sys
 import random
+import time
 
 out_dir = sys.argv[1]
 cc_file = sys.argv[2]
 h_file = sys.argv[3]
+
+random.seed(time.time())
 randNum = random.randint(0, 54321)
 
 def write_if_changed(path, contents):
-    if not os.path.exists(path):
-        with open(path, 'w') as fp:
-            fp.write(contents)
-        return
-
-    with open(path) as old_fp:
-        old_contents = old_fp.read()
-
-    if contents != old_contents:
-        with open(path, 'w') as fp:
-            fp.write(contents)
+  with open(path, 'w') as fp:
+    fp.write(contents)
+  return
 
 
-write_if_changed(f'{out_dir}/{cc_file}', f'''\
-#include "{h_file}"
-
+write_if_changed(f'{out_dir}/{h_file}', f'''\
 const char *world() {{
     return "world-{randNum}";
 }}
 ''')
 
-write_if_changed(f'{out_dir}/{h_file}', '''\
-const char *world();
-''')
